815a8b170a785cb24dc096a0055997eaec019c90
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2023-08-08 16:52:53 -0300

    cm

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,6 @@ function Footer(){
     return(
         <div className="container-footer">
             <div className="block blockA"><p>Brasil</p></div>
-            <div className="line"></div>
             <div className="block blockB"><p>Sobre</p><p>Publicidade</p><p>Negócios</p><p>Como funciona a Pesquisa</p></div>
         </div>
     )
